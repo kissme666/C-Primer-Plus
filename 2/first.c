4d463570866ce0deeea5231a107b086cb053f374
air <m787715894@gmail.com>
2019-01-06 23:48:09 +0800

    第二章示例code和课后习题code

---
+++ 2/first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    num = 1;
+
+
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favourite number is %d because is is first.\n", num);
+
+
+    return 0;
+}
