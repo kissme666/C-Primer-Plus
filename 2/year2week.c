d74e2ddc028e06befe41aed945ed386ccead2feb
air <m787715894@gmail.com>
2019-01-09 09:51:55 +0000

    第二章复习题第四题改错

---
+++ 2/year2week.c
@@ -0,0 +1,12 @@
+// year2week.c -- 该程序打印一年有多少周
+// 注意该程序是p36-4的课后复习题
+#include <stdio.h>
+int main(void)
+{
+    int s;
+    s=52;
+
+    printf("There are %d weeks in a year\n", s);
+
+    return 0;
+}
