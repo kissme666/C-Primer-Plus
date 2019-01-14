fb110215c7624845bc00323b40e4e095775bb999
air <m787715894@gmail.com>
2019-01-14 09:02:06 +0000

    使用固定宽度打印美观列表

---
+++ 4/printf1.c
@@ -0,0 +1,11 @@
+// printf1.c -- 使用固定宽度打印美观列表
+#include <stdio.h>
+int main(void)
+
+{
+    printf("%9d %9d %9d\n", 12, 234, 1222);
+    printf("%9d %9d %9d\n", 4, 5, 23);
+    printf("%9d %9d %9d\n", 22334, 2322, 1001);
+
+    return 0;
+}
