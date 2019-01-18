c7c3f198e862128ced164d4d593b3bf5b4f1b071
air <m787715894@gmail.com>
2019-01-18 08:04:11 +0000

    do while 示例 while 循环版本

---
+++ 6/entry.c
@@ -0,0 +1,22 @@
+// entry.c -- do_whilec.c while 循环版
+#include <stdio.h>
+int main(void)
+{
+    const int secret_code = 13;
+    int code_entered;
+
+
+    printf("To enter te triskaidekaphobia therapy club,\n");
+    printf("Please enter the secret code number:");
+    scanf("%d", &code_entered);
+    while( code_entered != secret_code)
+    {
+        printf("To enter te triskaidekaphobia therapy club,\n");
+        printf("Please enter the secret code number:");
+        scanf("%d", &code_entered);
+    }
+    printf("Congratulations! You are cured!\n");
+
+    return 0;
+}
+
